91a863c7a97a6c7bf16886d2cdca285644ddd207
Nikolay Chernov <nchernov@gmail.com>
2020-08-12 21:00:09 +0300

    UI fixes

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 class BaseConfig(object):
     """Base configuration."""
 
-    APP_NAME = "Eleza App"
+    APP_NAME = 'Eleza App'
+    SITE_NAME = "Eleza"
     DEBUG_TB_ENABLED = False
     SECRET_KEY = os.environ.get(
         "SECRET_KEY", "Ensure you set a secret key, this is important!"
