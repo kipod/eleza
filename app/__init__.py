192d6e327f2d4be161a588a110dce90a72950051
LikaDevelop <likakniazieva@gmail.com>
2020-08-13 19:51:07 +0300

    Landing demo 2 slide

--- app/__init__.py
+++ app/__init__.py
@@ -10,18 +10,19 @@ login_manager = LoginManager()
 db = SQLAlchemy()
 
 
-def create_app(environment='development'):
+def create_app(environment="development"):
 
     from config import config
     from .views import main_blueprint
     from .auth.views import auth_blueprint
+    from .demo.views import demo_blueprint
     from .auth.models import User, AnonymousUser
 
     # Instantiate app.
     app = Flask(__name__)
 
     # Set app config.
-    env = os.environ.get('FLASK_ENV', environment)
+    env = os.environ.get("FLASK_ENV", environment)
     app.config.from_object(config[env])
     config[env].configure(app)
 
@@ -32,19 +33,20 @@ def create_app(environment='development'):
     # Register blueprints.
     app.register_blueprint(auth_blueprint)
     app.register_blueprint(main_blueprint)
+    app.register_blueprint(demo_blueprint)
 
     # Set up flask login.
     @login_manager.user_loader
     def get_user(id):
         return User.query.get(int(id))
 
-    login_manager.login_view = 'auth.login'
-    login_manager.login_message_category = 'info'
+    login_manager.login_view = "auth.login"
+    login_manager.login_message_category = "info"
     login_manager.anonymous_user = AnonymousUser
 
     # Error handlers.
     @app.errorhandler(HTTPException)
     def handle_http_error(exc):
-        return render_template('error.html', error=exc), exc.code
+        return render_template("error.html", error=exc), exc.code
 
     return app
