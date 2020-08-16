62da6ab3949646b0659de50e025d09d62320588f
LikaDevelop <likakniazieva@gmail.com>
2020-08-16 16:33:43 +0300

    create helthcare page

--- app/demo/views.py
+++ app/demo/views.py
@@ -6,3 +6,8 @@ demo_blueprint = Blueprint("demo", __name__)
 @demo_blueprint.route("/demo")
 def demo():
     return render_template("demo.html")
+
+
+@demo_blueprint.route("/slide_helthcare_criteries")
+def slide_helthcare_criteries():
+    return render_template("slide2_for_criteries/helthcare_criteres.html")
