cc8dcdb51342a10fe5d113999751e7a8ad91e977
LikaDevelop <likakniazieva@gmail.com>
2020-08-17 19:27:57 +0300

    front table of prediction power

--- app/demo/views.py
+++ app/demo/views.py
@@ -1,4 +1,6 @@
 from flask import render_template, Blueprint
+from app.models import Feature, Subdomain
+from app.contoller import predictive_power
 
 demo_blueprint = Blueprint("demo", __name__)
 
@@ -10,4 +12,11 @@ def demo():
 
 @demo_blueprint.route("/slide_helthcare_criteries")
 def slide_helthcare_criteries():
-    return render_template("slide2_for_criteries/helthcare_criteres.html")
+    features = Feature.query.all()
+    subdomain = Subdomain.query.filter(Subdomain.name == "Diabetic").first()
+    pred_pow = predictive_power(subdomain)
+    return render_template(
+        "slide2_for_criteries/helthcare_criteres.html",
+        features=features,
+        pred_pow=pred_pow,
+    )
