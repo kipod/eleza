b970ac41752e51ec7e1d15cf51987fb981bd3be4
LikaDevelop <likakniazieva@gmail.com>
2020-08-17 21:12:15 +0300

    healthcare

---
+++ app/demo/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import SelectField, SubmitField, HiddenField
+from wtforms.validators import DataRequired, Length
+from app.models import Subdomain, ModelType
+
+
+def get_subdomain_names():
+    """retrieve list of choices for SelectField
+    """
+    return [(s.name, s.name) for s in Subdomain.query.all()]
+
+
+def get_model_type_names():
+    """retrieve list of choices for SelectField
+    """
+    return [(m.name, m.name) for m in ModelType.query.all()]
+
+
+class SubdomainChoiceForm(FlaskForm):
+
+    subdomain_name = SelectField("Type of application subdomain:")
+    subdomain_type = HiddenField()
+    model_type = SelectField("Select Model Type:")
+    next = SubmitField("Next")
--- app/demo/views.py
+++ app/demo/views.py
@@ -1,13 +1,26 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request, session, redirect, url_for, flash
 from app.models import Feature, Subdomain
 from app.contoller import predictive_power
+from .forms import SubdomainChoiceForm
+
 
 demo_blueprint = Blueprint("demo", __name__)
 
 
 @demo_blueprint.route("/demo")
 def demo():
-    return render_template("demo.html")
+    form = SubdomainChoiceForm(request.form)
+    form.subdomain_type.data = Subdomain.Type.healthcare.name
+    form.subdomains = Subdomain.query.all()
+    if form.validate_on_submit():
+        session["subdomain"] = form.subdomain_name.data
+        session["model_type"] = form.model_type.data
+        if form.subdomain_type.data == Subdomain.Type.healthcare.name:
+            redirect(url_for("demo.slide_helthcare_criteries"))
+    elif form.is_submitted():
+        flash("Invalid data", "warning")
+
+    return render_template("demo.html", form=form)
 
 
 @demo_blueprint.route("/slide_helthcare_criteries")
