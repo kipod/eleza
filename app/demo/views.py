d64abcdcd868e7457924aefe8c1df692bc868975
Vitaly <dagger180@gmail.com>
2020-09-03 11:48:54 +0300

    Proportional to...

--- app/demo/views.py
+++ app/demo/views.py
@@ -292,6 +292,13 @@ def explanations_per_patient(case_id):
                 one_square_val = sum(sum_explainer_abs.values()) / 12
                 square_num = int(round(abs(case_val.explainer) / one_square_val, 0))
                 form.table_rows[row_index] += [[feature_name, gen_squares_code(square_num)]]
+            else:
+                each_category_squares = 12 / len(form.categories)
+                value_of_square = int(round(100 / each_category_squares, 0))
+                sum_explainer_category = sum_explainer_abs[cat_name]
+                feature_explainer = int((case_val.explainer / sum_explainer_category) * 100)
+                feature_squares = int(round((feature_explainer / value_of_square), 0))
+                form.table_rows[row_index] += [[feature_name, gen_squares_code(feature_squares)]]
             row_index += 1
         for i in range(row_index, num_of_rows):
             if len(form.table_rows) <= i:
@@ -328,6 +335,7 @@ def explanations_per_patient(case_id):
 
 # Begin financial domain!
 
+
 @demo_blueprint.route("/financial_select_features", methods=["GET", "POST"])
 def financial_select_features():
     form = FinancialSelectFeatures(request.form)
@@ -345,9 +353,167 @@ def financial_select_features():
                 selected_features += [name]
         session["selected_features"] = selected_features
         session["ranges_for_feature"] = {}
-        return redirect(url_for("demo.range_groups"))
+        return redirect(url_for("demo.financial_range_groups"))
     elif form.is_submitted():
         flash("Invalid data", "warning")
     return render_template(
         "financial_select_features.html", features=features, pred_pow=pred_pow, form=form,
+    )
+
+
+@demo_blueprint.route("/financial_range_groups", methods=["GET", "POST"])
+def financial_range_groups():
+    form = RangeGroupsForm(request.form)
+    form.selected_features = session.get("selected_features", [])
+    form.subdomain = Subdomain.query.get(session.get("subdomain", None))
+    user_data_id = session["user_data_id"]
+    form.feature.choices = [(v, v) for v in form.selected_features]
+    form.ranges = {}
+    ranges_for_feature = session.get("ranges_for_feature", {})
+    for feature_name in form.selected_features:
+        feature = Feature.query.filter(Feature.name == feature_name).first()
+        all_values = [
+            c.value
+            for c in CaseValue.query.filter(CaseValue.user_data_id == user_data_id)
+            .filter(CaseValue.feature == feature)
+            .all()
+        ]
+        form.ranges[feature_name] = (min(all_values), max(all_values))
+    if form.validate_on_submit():
+        if form.next.data:
+            session["categories"] = {}
+            return redirect(url_for("demo.financial_categories"))
+        try:
+            if form.feature.data in ranges_for_feature:
+                ranges_for_feature[form.feature.data] += [
+                    (float(form.range_from.data), float(form.range_to.data))
+                ]
+            else:
+                ranges_for_feature[form.feature.data] = [
+                    (float(form.range_from.data), float(form.range_to.data))
+                ]
+            session["ranges_for_feature"] = ranges_for_feature
+        except ValueError:
+            flash("Invalid data", "warning")
+    elif form.is_submitted():
+        flash("Invalid data", "warning")
+
+    return render_template(
+        "financial_range_groups.html", form=form, ranges_for_feature=ranges_for_feature
+    )
+
+
+@demo_blueprint.route("/financial_categories", methods=["GET", "POST"])
+def financial_categories():
+    form = CategoriesForm(request.form)
+    form.selected_features = session.get("selected_features", [])
+    form.categories = session.get("categories", {})
+    form.subdomain = Subdomain.query.get(session.get("subdomain", None))
+    # ranges_for_feature = session.get("ranges_for_feature", {})
+    if form.validate_on_submit():
+        if form.submit.data:
+            form.categories[form.category_name.data] = [
+                k for k in request.form if request.form[k] == "on"
+            ]
+            session["categories"] = form.categories
+        if form.next.data:
+            session["categories"] = form.categories
+            # {}
+            return redirect(url_for("demo.financial_explan_summary"))
+    elif form.is_submitted():
+        flash("Invalid data", "warning")
+    return render_template("financial_categories.html", form=form)
+
+# financial_explan_summary
+
+
+@demo_blueprint.route("/financial_explan_summary", methods=["GET", "POST"])
+def financial_explan_summary():
+    form = FlaskForm(request.form)
+    categories = session.get("categories", {})
+    form.subdomain = Subdomain.query.get(session.get("subdomain", None))
+    ranges_for_feature = session.get("ranges_for_feature", {})
+    range_groups_ages = ranges_for_feature.get("Age", [])
+    form.presentation_type = "Percents"
+    if form.validate_on_submit():
+        form.presentation_type = request.form["presentation_type"]
+    elif form.is_submitted():
+        flash("Invalid data", "warning")
+
+    form.table_heads = [
+        "Patient ID",
+        "Age",
+        f"{form.subdomain.name} Predicted",
+        "Prediction or Confidence Score (Out of 100)",
+    ]
+
+    form.table_heads += [f"{name} Contribution" for name in categories]
+
+    all_case_values_query = CaseValue.query.filter(
+        CaseValue.user_data_id == session["user_data_id"]
+    )
+    max_patient_id = max([v.case_id for v in all_case_values_query.all()])
+    form.table_rows = []
+    for patient_id in range(max_patient_id + 1):
+        all_case_values_query_for_patient = all_case_values_query.filter(
+            CaseValue.case_id == patient_id
+        )
+        if not all_case_values_query_for_patient.all():
+            continue
+        age_feature = Feature.query.filter(Feature.name == "Age").first()
+        age_case_val = all_case_values_query_for_patient.filter(
+            CaseValue.feature_id == age_feature.id
+        ).first()
+        age = int(age_case_val.value)
+        age = age_range_groups(range_groups_ages, age)
+        prediction_score = int(round(age_case_val.prediction, 2) * 100)
+        predicted = "No" if prediction_score < 50 else "Yes"
+        prediction_score_color = "red"
+        if prediction_score > 45:
+            prediction_score_color = "yellow"
+            if prediction_score > 70:
+                prediction_score_color = "green"
+
+        patient_id_bgcolor = "MediumSlateBlue"
+        row = [
+            (patient_id, patient_id_bgcolor),
+            (age, None),
+            (predicted, None),
+            (prediction_score, prediction_score_color),
+        ]
+        explainers = []
+        for cat_name in categories:
+            sum_explainer = 0
+            for feature_name in categories[cat_name]:
+                feature = Feature.query.filter(Feature.name == feature_name).first()
+                case_val = all_case_values_query_for_patient.filter(
+                    CaseValue.feature_id == feature.id
+                ).first()
+                sum_explainer += case_val.explainer
+            explainers += [sum_explainer]
+        # if not selected percentage
+        cells = None
+
+        if form.presentation_type == "Percents":
+            hundred = sum(explainers)
+            values = list(
+                map(lambda val: int(round(val * 100 / hundred, 0)), explainers)
+            )
+            max_val = max(values)
+            colors = list(
+                map(lambda val: ("green" if val == max_val else None), values)
+            )
+            cells = list(zip(values, colors))
+        else:
+            values = list(map(lambda val: (round(val, 4)), explainers))
+            max_val = max(values)
+            colors = list(
+                map(lambda val: ("green" if val == max_val else None), values)
+            )
+            cells = list(zip(values, colors))
+        row += cells
+        form.table_rows += [row]
+
+    return render_template(
+        "financial_explan_summary.html", form=form, range_groups_ages=range_groups_ages,
     )
